c63601fe47b46828af5ccdfa771fd20304304bd6
RazvanIonut97 <48863405+RazvanIonut97@users.noreply.github.com>
2021-08-21 00:49:56 +0300

    make it better

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ mongoose.connect(process.env.DB_CONNECTION, {
     })
     .catch(err => console.log(err))
 //Listening to the server
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on port http://localhost:${port}`));
